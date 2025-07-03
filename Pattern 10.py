d1166f1353ecf0347b432819c68b9e1b1da7e0f5
Parasiticwire <zain.matloob@gmail.com>
2025-07-03 11:22:26 +0500

    Add files via upload

---
+++ Pattern 10.py
@@ -0,0 +1,4 @@
+print(' ** ** ')
+print('**** ****')
+for i in range(5, 0, -2):
+    print(' '*((5-i)//2) + '*'*i)
---
+++ Pattern 9.py
@@ -0,0 +1,5 @@
+n = 5
+print(' '*(n-1) + '*')
+for i in range(2, n):
+    print(' '*(n-i) + '*' + ' '*(2*i-3) + '*')
+print('*'*(2*n-1))
